49b5dbc81701409e3847b6b3b985e4f9786fc378
RichardFelipe-collab <richardfelipe.123@hotmail.com>
2022-06-02 21:05:46 -0300

    primeiro modulo finalizado

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ReactRefreshWeboackPlugin = require('@react-refresh-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV != 'production';
 
 module.exports = {
   mode:isDevelopment?'development':'production',
   devtool:isDevelopment?'eval-source-map': 'source-map',
-  entry:path.resolve(__dirname, 'src','index.jsx'),
+  entry:path.resolve(__dirname, 'src','index.tsx'),
   output:{
     path: path.resolve(__dirname,'dist'),
     filename:'bundle.js'
   },
   resolve:{
-    extensions:['.js', '.jsx'],
+    extensions:['.js', '.jsx','.ts','.tsx'],
   },
   devServer:{
     static: path.resolve(__dirname, 'public'),
   },
   plugins: [
-    isDevelopment && new ReactRefreshWebpackPlugin,
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public','index.html')
     })
@@ -27,7 +27,7 @@ module.exports = {
   module:{
     rules:[
       {
-        test:/\.jsx$/,
+        test:/\.(j|t)sx$/,
         exclude:/node_modules/,
         use:{
           loader:'babel-loader',
